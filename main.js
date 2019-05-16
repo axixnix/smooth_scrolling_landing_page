6d3e9d9b6203bc0ad1a5ce8ea0311e35105b0847
axixnix <henryirons@yahoo.com>
2019-05-16 20:39:31 +0100

    jQuery smooth scroll completed

--- main.js
+++ main.js
@@ -3,5 +3,9 @@ $('.navbar a').on('click', function (e) {
   if (this.hash !== '') {
     e.preventDefault()
     const hash = this.hash
+    $('html,body').animate(
+      {scrollTop: $(hash).offset().top},
+      800 // this is for the speed
+    )
   }
 })
